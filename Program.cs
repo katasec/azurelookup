43b732a6d6776d670d95948d7c220cbe4f709e17
Ameer Deen <writeameer@gmail.com>
2023-02-16 22:13:46 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.Storage;
+using Pulumi.AzureNative.Storage.Inputs;
+using System.Collections.Generic;
+
+return await Pulumi.Deployment.RunAsync(() =>
+{
+
+
+    // Export the primary key of the Storage Account
+    return new Dictionary<string, object?>
+    {
+        ["message"] = "Hello World"
+    };
+});
