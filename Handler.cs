67e8c18a532eef3ac0b18b0545fd7032e57e0ac7
Ameer Deen <writeameer@gmail.com>
2023-02-21 20:31:53 +0400

    Moved lookup to separate class

---
+++ Handler.cs
@@ -0,0 +1,30 @@
+﻿using Pulumi.AzureNative.Resources;
+using System.Collections.Generic;
+
+namespace azurelookup;
+
+
+public class Handler
+{
+    public static Dictionary<string, object?> Lookup()
+    {
+        // Setup args for GetResourceGroup function
+        var args = new GetResourceGroupInvokeArgs { ResourceGroupName = "rg-ea-azurearc" };
+
+        // Invoke GetResourceGroup function
+        var group = GetResourceGroup.Invoke(args, null);
+
+        var group2 = GetResourceGroup.Invoke(new GetResourceGroupInvokeArgs { ResourceGroupName = "rg-ea-azurearc" }, null);
+
+        // Get Resource Group Attributges
+        var name = group.Apply(x => x.Name);
+        var Id = group.Apply(x => x.Id);
+
+        // Export attributes to view on console
+        return new Dictionary<string, object?>
+        {
+            ["group"] = name,
+            ["Id"] = Id,
+        };
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,26 +1,5 @@
-﻿using Pulumi;
-using Pulumi.AzureNative.Resources;
-using Pulumi.AzureNative.Storage;
-using Pulumi.AzureNative.Storage.Inputs;
-using System.Collections.Generic;
-
-return await Pulumi.Deployment.RunAsync(() =>
+﻿return await Pulumi.Deployment.RunAsync(() =>
 {
-
-    // Setup args for GetResourceGroup function
-    var args = new GetResourceGroupInvokeArgs { ResourceGroupName = "rg-ea-azurearc" };
-
-    // Invoke GetResourceGroup function
-    var group = GetResourceGroup.Invoke(args,null);
-
-    // Get Resource Group Attributges
-    var name = group.Apply(x => x.Name);
-    var Id = group.Apply(x => x.Id);
-    
-    // Export attributes to view on console
-    return new Dictionary<string, object?>
-    {
-        ["group"] =  name,
-        ["Id"] = Id,
-    };
+    return azurelookup.Handler.Lookup();
 });
+
