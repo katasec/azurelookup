ad03ca715fc533cae638fa08c282d0ed9f88c94b
Ameer Deen <writeameer@gmail.com>
2023-02-21 21:06:23 +0400

    Added subnet lookup

--- Handler.cs
+++ Handler.cs
@@ -1,11 +1,23 @@
-﻿using Pulumi.AzureNative.Resources;
+﻿using Pulumi.AzureNative.RecoveryServices.Inputs;
+using Pulumi.AzureNative.Resources;
 using System.Collections.Generic;
+using AzureNative = Pulumi.AzureNative;
+using Network = Pulumi.AzureNative.Network;
 
 namespace azurelookup;
 
 
 public class Handler
 {
+    public static void LookupSubnet()
+    {
+        var x = Network.GetSubnet.Invoke(new()
+        {
+            SubnetName = "blah",
+            VirtualNetworkName = "blah",
+            ResourceGroupName = "blah"
+        });
+    }
     public static Dictionary<string, object?> Lookup()
     {
         // Setup args for GetResourceGroup function
@@ -14,7 +26,9 @@ public class Handler
         // Invoke GetResourceGroup function
         var group = GetResourceGroup.Invoke(args, null);
 
-        var group2 = GetResourceGroup.Invoke(new GetResourceGroupInvokeArgs { ResourceGroupName = "rg-ea-azurearc" }, null);
+        var group2 = GetResourceGroup.Invoke(new() { 
+            ResourceGroupName = "rg-ea-azurearc" 
+        }, null);
 
         // Get Resource Group Attributges
         var name = group.Apply(x => x.Name);
